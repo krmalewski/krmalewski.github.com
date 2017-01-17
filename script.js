761286f209b5f02e7d576f59b29b94c100665078
Krystyna Malewski <krmalewski@gmail.com>
2017-01-17 17:53:55 -0500

    animate icons

--- script.js
+++ script.js
@@ -7,14 +7,33 @@ $(() => {
   $('.land').css('height', windowHeight);
 
   const header = $('.header');
-  header[0].classList.add('slideIn');
+  setTimeout(() => {
+    header[0].classList.add('slideIn');
+  }, 1000);
 
   const github = $('#github-logo');
   const email = $('#email-logo');
   const linkedIn = $('#linked-in');
 
+  setTimeout(() => {
+    email.addClass('animated2 fadeInUp');
+  }, 1200);
+
+  setTimeout(() => {
+    github.addClass('animated2 fadeInUp');
+  }, 1400);
+
+  setTimeout(() => {
+    linkedIn.addClass('animated2 fadeInUp');
+  }, 1600);
+
   github.on('mouseenter', function() {
-    github.addClass('animated bounce')
+    github.css({
+      'opacity': '0.6',
+      'transform': 'none'
+    });
+    github.removeClass('animated2 fadeInUp');
+    github.addClass('animated bounce');
   })
 
   github.on('mouseout', function() {
@@ -23,6 +42,11 @@ $(() => {
   })
 
   email.on('mouseenter', function() {
+    email.css({
+      'opacity': '0.6',
+      'transform': 'none'
+    });
+    email.removeClass('animated2 fadeInUp');
     email.addClass('animated rubberBand')
   })
 
@@ -32,6 +56,11 @@ $(() => {
   })
 
   linkedIn.on('mouseenter', function() {
+    linkedIn.css({
+      'opacity': '0.6',
+      'transform': 'none'
+    });
+    linkedIn.removeClass('animated2 fadeInUp');
     linkedIn.addClass('animated jello')
   })
 
